71f66f97b58418484ca492a8c5f588d639374378
TheSeth0604 <alvarocrispin0604@gmail.com>
2024-05-03 15:23:02 -0500

    fix: adding projects to sidenav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProjectDashboardComponent} from "./management/pages/project-dashboard/project-dashboard.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'projects', component: ProjectDashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
