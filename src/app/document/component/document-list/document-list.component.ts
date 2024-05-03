080acb454ad2ecc77d0d84a982f3a18de80a6f44
TheSeth0604 <alvarocrispin0604@gmail.com>
2024-05-03 02:45:30 -0500

    feat: implemented list of documents

--- src/app/document/component/document-list/document-list.component.ts
+++ src/app/document/component/document-list/document-list.component.ts
@@ -1,24 +1,66 @@
-import { Component, OnInit } from '@angular/core';
-import { Document } from '../../model/document';
-import {DocumentService} from "../../service/documents-api.service"; // Importamos la clase Document
+import {Component, OnInit, Input} from '@angular/core';
+import {Document} from "../../model/document.entity";
+import {DocumentService} from "../../service/documents-api.service";
+import {MatDialog} from "@angular/material/dialog";
+import {AddDocumentDialogComponent} from "../add-delete-edit-document-dialogs/add-document-dialog/add-document-dialog.component";
+import {EditDocumentDialogComponent} from "../add-delete-edit-document-dialogs/edit-document-dialog/edit-document-dialog.component";
+import {DeleteDocumentDialogComponent} from "../add-delete-edit-document-dialogs/delete-document-dialog/delete-document-dialog.component";
 
 @Component({
   selector: 'app-document-list',
   templateUrl: './document-list.component.html',
   styleUrls: ['./document-list.component.css']
 })
-export class DocumentListComponent implements OnInit {
-  documents: Document[] = []; // Array para almacenar los documentos
+export class DocumentListComponent implements OnInit{
 
-  constructor(private documentService: DocumentService) { }
+  @Input() projectId!: number;
+  documents: Document[] = [];
+
+  constructor(private dialog: MatDialog, private documentService: DocumentService) { }
 
   ngOnInit(): void {
-    this.getDocuments();
+    this.loadDocuments();
   }
 
-  getDocuments(): void {
-    this.documentService.getDocuments()
-      .subscribe(documents => this.documents = documents);
+  loadDocuments() {
+    this.documentService.getDocumentsByProject(this.projectId)
+      .subscribe(documents => {
+        this.documents = documents;
+      });
+  }
+
+  openAddDialog() {
+    const dialogRef = this.dialog.open(AddDocumentDialogComponent, {
+      data: { projectId: this.projectId }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+      this.loadDocuments();
+    });
+  }
+
+  openEditDialog(documentId: number): void {
+    if (documentId) {
+      const dialogRef = this.dialog.open(EditDocumentDialogComponent, {
+        data: {documentId: documentId}
+      });
+
+      dialogRef.afterClosed().subscribe(result => {
+        console.log(`Dialog result: ${result}`);
+        this.loadDocuments();
+      });
+    }
+  }
+
+  openDeleteDialog(documentId: number) {
+    const dialogRef = this.dialog.open(DeleteDocumentDialogComponent, {
+      data: { documentId: documentId }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.loadDocuments();
+    });
   }
 }
 
