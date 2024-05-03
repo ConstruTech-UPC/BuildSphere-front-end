cd04054a8cddb8c2af1a7c0946026b6bd48ab8b8
CamilaLoli <u202110385@upc.edu.pe>
2024-05-03 03:55:35 -0500

    fix: app.component and services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {InventoryManagementComponent} from "./inventory/pages/inventory-management/inventory-management.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inventory', component: InventoryManagementComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,61 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import {HttpClientModule} from "@angular/common/http";
 import {MaterialsService} from "./inventory/services/materials.service";
 import {MachineryService} from "./inventory/services/machinery.service";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
+import {MatTableModule} from "@angular/material/table";
+import {MatSortModule} from "@angular/material/sort";
+import {MatIconModule} from "@angular/material/icon";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatCardModule} from "@angular/material/card";
+import {MatGridListModule} from "@angular/material/grid-list";
+import { AddDeleteEditMaterialComponent } from './inventory/components/add-delete-edit-material/add-delete-edit-material.component';
+import { AddDeleteEditMachineComponent } from './inventory/components/add-delete-edit-machine/add-delete-edit-machine.component';
+import { MachineryTableComponent } from './inventory/components/machinery-table/machinery-table.component';
+import { MaterialsTableComponent } from './inventory/components/materials-table/materials-table.component';
+import {InventoryManagementComponent} from "./inventory/pages/inventory-management/inventory-management.component";
+import { AddMachineDialogComponent } from './inventory/components/add-delete-edit-machine-dialogs/add-machine-dialog/add-machine-dialog.component';
+import { EditMachineDialogComponent } from './inventory/components/add-delete-edit-machine-dialogs/edit-machine-dialog/edit-machine-dialog.component';
+import { DeleteMachineDialogComponent } from './inventory/components/add-delete-edit-machine-dialogs/delete-machine-dialog/delete-machine-dialog.component';
+import { AddMaterialDialogComponent } from './inventory/components/add-delete-edit-material-dialogs/add-material-dialog/add-material-dialog.component';
+import { EditMaterialDialogComponent } from './inventory/components/add-delete-edit-material-dialogs/edit-material-dialog/edit-material-dialog.component';
+import { DeleteMaterialDialogComponent } from './inventory/components/add-delete-edit-material-dialogs/delete-material-dialog/delete-material-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InventoryManagementComponent,
+    AddDeleteEditMaterialComponent,
+    AddDeleteEditMachineComponent,
+    MachineryTableComponent,
+    MaterialsTableComponent,
+    AddMachineDialogComponent,
+    EditMachineDialogComponent,
+    DeleteMachineDialogComponent,
+    AddMaterialDialogComponent,
+    EditMaterialDialogComponent,
+    DeleteMaterialDialogComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatTableModule,
+    MatSortModule,
+    MatIconModule,
+    MatPaginatorModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatGridListModule
+
   ],
   providers: [
     MaterialsService,
