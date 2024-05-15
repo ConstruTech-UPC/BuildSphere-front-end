e3d1458aca4c95dfff8cd013f3688bc190cbcd08
Ventura Chancafe, Eduardo Renato <eduventura7777@gmail.com>
2024-05-15 13:26:43 -0500

    Revert "Feature/project management"

--- src/app/management/model/project.entity.ts
+++ src/app/management/model/project.entity.ts
@@ -1,11 +1,5 @@
 export class Project {
   id: number;
-  task_id: number;
-  team_id: number;
-  worker_id: number;
-  document_id: number;
-  machine_id: number;
-  material_id: number;
   name: string;
   description: string;
   location: string;
@@ -15,12 +9,6 @@ export class Project {
   urlImage: string;
   constructor() {
     this.id = 0;
-    this.task_id = 0;
-    this.team_id = 0;
-    this.worker_id = 0;
-    this.document_id = 0;
-    this.machine_id = 0;
-    this.material_id = 0;
     this.name = '';
     this.description = '';
     this.location = '';
