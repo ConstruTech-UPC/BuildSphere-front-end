ddd89a7654d87eb9ed88063dd9cf709a33f10a9e
acrispin06 <alvarocrispin0604@gmail.com>
2024-05-22 18:19:52 -0500

    feat: implementing list project management

--- src/app/public/components/project-information-management/project-information-management.component.ts
+++ src/app/public/components/project-information-management/project-information-management.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-project-information-management',
   templateUrl: './project-information-management.component.html',
   styleUrls: ['./project-information-management.component.css']
 })
-export class ProjectInformationManagementComponent { }
+export class ProjectInformationManagementComponent {
+
+  constructor(private router: Router) {}
+
+  onProjectSelect(projectId: number) {
+    this.router.navigate(['/projects', projectId]);
+  }
+}
