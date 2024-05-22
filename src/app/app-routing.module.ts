9a6301e6305e21aa07df95f9fd680fbe8186c37c
acrispin06 <alvarocrispin0604@gmail.com>
2024-05-22 18:18:50 -0500

    feat: updating to show the list of projects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DocumentsManagementComponent } from "./document/pages/documents-management/documents-management.component";
-import { CollaborationManagementComponent } from "./colaboration/pages/collaboration-management/collaboration-management.component";
-import { InventoryManagementComponent } from "./inventory/pages/inventory-management/inventory-management.component";
-import { ProjectDashboardComponent } from "./management/pages/project-dashboard/project-dashboard.component";
+import { ProjectDashboardComponent } from './management/pages/project-dashboard/project-dashboard.component';
+import { DocumentsManagementComponent } from './document/pages/documents-management/documents-management.component';
+import { CollaborationManagementComponent } from './colaboration/pages/collaboration-management/collaboration-management.component';
+import { InventoryManagementComponent } from './inventory/pages/inventory-management/inventory-management.component';
 import {
   ProjectInformationManagementComponent
 } from "./public/components/project-information-management/project-information-management.component";
 
 const routes: Routes = [
-  { path: '', component: ProjectDashboardComponent },
-  { path: 'projects', component: ProjectDashboardComponent },
+  { path: '', component: ProjectInformationManagementComponent },
+  { path: 'projects', component: ProjectInformationManagementComponent },
   {
-    path: 'projects/:projectId', component: ProjectInformationManagementComponent, children: [
+    path: 'projects/:projectId', component: ProjectDashboardComponent, children: [
       { path: 'documents', component: DocumentsManagementComponent },
       { path: 'collaboration', component: CollaborationManagementComponent },
       { path: 'inventory', component: InventoryManagementComponent }
