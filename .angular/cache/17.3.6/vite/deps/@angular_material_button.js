59f97950110d3e57e10bf321b2fbd7d17469f88c
PieroM1 <u201923446@upc.edu.pe>
2024-05-23 22:43:52 -0500

    merge: featureDocuments into featureCollaboration

---
+++ .angular/cache/17.3.6/vite/deps/@angular_material_button.js
@@ -0,0 +1,33 @@
+import {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+} from "./chunk-H5JLT5GZ.js";
+import "./chunk-5CJLD3XF.js";
+import "./chunk-YVUGXVXU.js";
+import "./chunk-7G4ISVJH.js";
+import "./chunk-SXIXOCJ4.js";
+export {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+};
+//# sourceMappingURL=@angular_material_button.js.map
---
+++ .angular/cache/17.3.6/vite/deps/@angular_material_divider.js
@@ -0,0 +1,13 @@
+import {
+  MatDivider,
+  MatDividerModule
+} from "./chunk-KK5HK2UC.js";
+import "./chunk-5CJLD3XF.js";
+import "./chunk-YVUGXVXU.js";
+import "./chunk-7G4ISVJH.js";
+import "./chunk-SXIXOCJ4.js";
+export {
+  MatDivider,
+  MatDividerModule
+};
+//# sourceMappingURL=@angular_material_divider.js.map
