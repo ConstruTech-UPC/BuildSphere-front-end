b7f7ab2ecd380dc36e42141204a3c4e3d35f82b6
acrispin06 <alvarocrispin0604@gmail.com>
2024-05-23 19:25:24 -0500

    fix: changed to emmit the correct projectId

--- src/app/management/components/project-card/project-card.component.ts
+++ src/app/management/components/project-card/project-card.component.ts
@@ -1,23 +1,30 @@
-import {Component, Input,Output, EventEmitter} from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Project } from '../../model/project.entity';
 
 @Component({
   selector: 'app-project-card',
   templateUrl: './project-card.component.html',
-  styleUrl: './project-card.component.css'
+  styleUrls: ['./project-card.component.css']
 })
 export class ProjectCardComponent {
   @Input() project: Project = new Project();
   @Output() edit = new EventEmitter<Project>();
   @Output() delete = new EventEmitter<number>();
+  @Output() titleClick = new EventEmitter<number>(); // Add this line
 
   constructor() {}
 
-  onEdit(): void {
+  onEdit(event: Event): void {
+    event.stopPropagation(); // Prevent the event from bubbling up
     this.edit.emit(this.project);
   }
 
-  onDelete(): void {
+  onDelete(event: Event): void {
+    event.stopPropagation(); // Prevent the event from bubbling up
     this.delete.emit(this.project.id);
+  }
+
+  onTitleClick(): void { // Add this method
+    this.titleClick.emit(this.project.id);
   }
 }
