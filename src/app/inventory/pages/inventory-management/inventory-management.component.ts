8941e7946153da1703c2de4409f6a9b17cdb8db7
CamilaLoli <u202110385@upc.edu.pe>
2024-05-27 22:42:40 -0500

    fix(inventory-management-component): added functions for the calculation of budget from each project.

--- src/app/inventory/pages/inventory-management/inventory-management.component.ts
+++ src/app/inventory/pages/inventory-management/inventory-management.component.ts
@@ -2,6 +2,10 @@ import {Component, OnInit} from '@angular/core';
 import {MachineryService} from "../../services/machinery.service";
 import {MaterialsService} from "../../services/materials.service";
 import {ActivatedRoute} from "@angular/router";
+import {Project} from "../../../management/model/project.entity";
+import {ProjectsService} from "../../../management/services/projects.service";
+import {Material} from "../../model/material.entity";
+import {Machine} from "../../model/machine.entity";
 
 @Component({
   selector: 'app-inventory-management',
@@ -10,9 +14,17 @@ import {ActivatedRoute} from "@angular/router";
 })
 export class InventoryManagementComponent implements OnInit {
   projectId!: number;
+  project: Project = new Project();
+  totalMaterialCost: number = 0;
+  totalMachineryCost: number = 0;
+  remainingBudget: number = 0;
 
   //constructor(private machineryService: MachineryService, private materialsService: MaterialsService) { }
-  constructor(private route: ActivatedRoute) {
+  constructor(
+      private route: ActivatedRoute,
+      private machineryService: MachineryService,
+      private materialsService: MaterialsService,
+      private projectService: ProjectsService) {
   }
 
   ngOnInit() {
@@ -20,6 +32,7 @@ export class InventoryManagementComponent implements OnInit {
       this.route.parent.params.subscribe(params => {
         this.projectId = +params['projectId'];
         if (!isNaN(this.projectId)) {
+            this.loadProjectData();
           console.log('Project ID: ', this.projectId);
         } else {
           console.error('Invalid Project ID: ', params['projectId']);
@@ -28,5 +41,34 @@ export class InventoryManagementComponent implements OnInit {
     } else {
       console.error('No parent route found.');
     }
+  }
+
+  loadProjectData() {
+    this.projectService.getProjectById(this.projectId)
+        .subscribe((project: Project) => {
+          this.project = project;
+          this.loadInventoryData();
+        });
+  }
+
+  loadInventoryData() {
+    this.materialsService.getMaterialsByProject(this.projectId)
+        .subscribe((materials: Material[]) => {
+          this.totalMaterialCost = materials.reduce((sum, material) =>
+              sum + material.totalCost, 0);
+          this.calculateRemainingBudget();
+            }
+        );
+
+    this.machineryService.getMachinesByProject(this.projectId)
+        .subscribe((machines: Machine[]) => {
+          this.totalMachineryCost = machines.reduce((sum, machine) =>
+          sum + machine.totalCost, 0);
+          this.calculateRemainingBudget();
+        });
+  }
+
+  calculateRemainingBudget() {
+    this.remainingBudget = this.project.budget - (this.totalMaterialCost + this.totalMachineryCost);
   }
 }
